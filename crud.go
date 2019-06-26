4c7da0074100f87fe459f9bcda31519afcb4df57
epsnv <epsnv@yandex.ru>
2019-06-26 06:30:11 +0500

    po kapelke

--- crud.go
+++ crud.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"net/http"
 )
 
@@ -18,19 +19,44 @@ type Messager struct {
 	DB *sql.DB
 }
 
+type Item struct {
+	id       int
+	user_id  int
+	subject  int
+	read     int
+	priority int
+}
+
 // тут вы должны уже писать ваши хендлеры
-func (m *Messager) List(http.ResponseWriter, *http.Request) {
+func (m *Messager) List(w http.ResponseWriter, r *http.Request) {
+	items := []*Item{}
+	rows, err := m.DB.Query("SELECT id, name, price, cnt, sum, store_count FROM products ORDER BY id ASC")
+	if err != nil {
+		w.Write([]byte(`{"status": 500, "error": "db error"}`))
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		it := &Item{}
+		err := rows.Scan(&it.id, &it.user_id, &it.subject, &it.read, &it.priority)
+		if err != nil {
+			w.Write([]byte(`{"status": 500, "error": "db error"}`))
+			return
+		}
+		items = append(items, it)
+	}
+	result, _ := json.Marshal(items)
+	w.Write(result)
+}
+
+func (m *Messager) Mark(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (m *Messager) Mark(http.ResponseWriter, *http.Request) {
+func (m *Messager) Delete(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (m *Messager) Delete(http.ResponseWriter, *http.Request) {
-
-}
-
-func (m *Messager) Create(http.ResponseWriter, *http.Request) {
+func (m *Messager) Create(w http.ResponseWriter, r *http.Request) {
 
 }
