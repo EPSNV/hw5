b385bde38629b141a54824015f1fe72213a03965
epsnv <epsnv@yandex.ru>
2019-06-26 17:59:29 +0500

    update

--- crud.go
+++ crud.go
@@ -22,29 +22,56 @@ type Messager struct {
 type Item struct {
 	id       int
 	user_id  int
-	subject  int
+	subject  string
 	read     int
 	priority int
 }
 
 // тут вы должны уже писать ваши хендлеры
 func (m *Messager) List(w http.ResponseWriter, r *http.Request) {
-	items := []*Item{}
-	rows, err := m.DB.Query("SELECT id, name, price, cnt, sum, store_count FROM products ORDER BY id ASC")
-	if err != nil {
-		w.Write([]byte(`{"status": 500, "error": "db error"}`))
+	session, err := r.Cookie("session_id")
+	if err == http.ErrNoCookie {
+		w.Write([]byte(`{"status": 501, "error": "db error"}`))
 		return
 	}
+
+	user, err := m.DB.Query("SELECT user_id FROM sessions WHERE id = $1", session.Value)
+	if err != nil {
+		w.Write([]byte(`{"status": 502, "error": "db error"}`))
+		return
+	}
+	defer user.Close()
+
+	user_val := 0
+	for user.Next() {
+		err = user.Scan(&user_val)
+		if err != nil {
+			w.Write([]byte(`{"status": 503, "error": "db error"}`))
+			return
+		}
+	}
+
+	items := []*Item{}
+	rows, err := m.DB.Query("SELECT id, user_id, subject, read, priority FROM messages WHERE user_id = $1 ORDER BY id ASC", user_val)
+
+	if err != nil {
+		w.Write([]byte(`{"status": 504, "error": "db error"}`))
+		return
+	}
+
 	defer rows.Close()
+
 	for rows.Next() {
 		it := &Item{}
 		err := rows.Scan(&it.id, &it.user_id, &it.subject, &it.read, &it.priority)
 		if err != nil {
-			w.Write([]byte(`{"status": 500, "error": "db error"}`))
+			w.Write([]byte(`{"status": 501, "error": "db error"}`))
 			return
 		}
+
 		items = append(items, it)
 	}
+
 	result, _ := json.Marshal(items)
 	w.Write(result)
 }
