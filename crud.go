50fc83022fba18073a2eb84d53087ca0faab86d5
epsnv <epsnv@yandex.ru>
2019-06-27 17:34:15 +0500

    go to home

--- crud.go
+++ crud.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -22,31 +23,6 @@ type Messager struct {
 type Item struct {
 	id       int
 	user_id  int
-<<<<<<< HEAD
-	subject  int
-	read     int
-	priority int
-}
-
-// тут вы должны уже писать ваши хендлеры
-func (m *Messager) List(w http.ResponseWriter, r *http.Request) {
-	items := []*Item{}
-	rows, err := m.DB.Query("SELECT id, name, price, cnt, sum, store_count FROM products ORDER BY id ASC")
-	if err != nil {
-		w.Write([]byte(`{"status": 500, "error": "db error"}`))
-		return
-	}
-	defer rows.Close()
-	for rows.Next() {
-		it := &Item{}
-		err := rows.Scan(&it.id, &it.user_id, &it.subject, &it.read, &it.priority)
-		if err != nil {
-			w.Write([]byte(`{"status": 500, "error": "db error"}`))
-			return
-		}
-		items = append(items, it)
-	}
-=======
 	subject  string
 	read     int
 	priority int
@@ -54,30 +30,69 @@ func (m *Messager) List(w http.ResponseWriter, r *http.Request) {
 
 // тут вы должны уже писать ваши хендлеры
 func (m *Messager) List(w http.ResponseWriter, r *http.Request) {
+	ReadMap := map[int]bool{
+		0: false,
+		1: true,
+	}
+
+	PriorityMap := map[int]string{
+		1: "low",
+		3: "normal",
+		5: "high",
+	}
+
 	session, err := r.Cookie("session_id")
 	if err == http.ErrNoCookie {
 		w.Write([]byte(`{"status": 501, "error": "db error"}`))
 		return
 	}
-
+	fmt.Println("session id")
+	var userV int
 	user, err := m.DB.Query("SELECT user_id FROM sessions WHERE id = $1", session.Value)
+	defer user.Close()
 	if err != nil {
-		w.Write([]byte(`{"status": 502, "error": "db error"}`))
+		fmt.Printf("error2 = %s \n", err.Error())
+		w.Write([]byte(`{"status": 503, "error": "db error"}`))
 		return
 	}
-	defer user.Close()
-
-	user_val := 0
+	fmt.Println("user id")
 	for user.Next() {
-		err = user.Scan(&user_val)
-		if err != nil {
-			w.Write([]byte(`{"status": 503, "error": "db error"}`))
-			return
-		}
+		user.Scan(&userV)
+		fmt.Println("Scan in userV")
 	}
+	// } else {
+	// 	fmt.Printf("error1 = %s \n", err.Error())
+	// 	w.Write([]byte(`{"status": 502, "error": "ErrNoRows"}`))
+	// 	return
+	// }
 
-	items := []*Item{}
-	rows, err := m.DB.Query("SELECT id, user_id, subject, read, priority FROM messages WHERE user_id = $1 ORDER BY id ASC", user_val)
+	// user, err := m.DB.Query("SELECT user_id FROM sessions WHERE id = $1", session.Value)
+
+	// if err != nil {
+	// 	w.Write([]byte(`{"status": 502, "error": "no auth"}`))
+	// 	return
+	// }
+	// defer user.Close()
+
+	// user_val := 0
+	// i := 0
+	// if user.Next() {
+	// 	err = user.Scan(&user_val)
+	// 	if err != nil {
+	// 		fmt.Printf("error2 = %s \n", err.Error())
+	// 		w.Write([]byte(`{"status": 503, "error": "db error"}`))
+	// 		return
+	// 	}
+	// 	i = i + 1
+	// } else {
+	// 	fmt.Printf("12341234243244")
+	// }
+	// if i == 0 {
+	// 	fmt.Printf("i not changed")
+	// }
+
+	items := []map[string]interface{}{}
+	rows, err := m.DB.Query("SELECT id, subject, read, priority FROM messages WHERE user_id = $1 ORDER BY id ASC", userV)
 
 	if err != nil {
 		w.Write([]byte(`{"status": 504, "error": "db error"}`))
@@ -85,20 +100,21 @@ func (m *Messager) List(w http.ResponseWriter, r *http.Request) {
 	}
 
 	defer rows.Close()
-
+	fmt.Println("message`s data")
 	for rows.Next() {
-		it := &Item{}
-		err := rows.Scan(&it.id, &it.user_id, &it.subject, &it.read, &it.priority)
+		//it := map[string]interface{}{}
+		var id, subject interface{}
+		var read, priority int
+		err := rows.Scan(&id, &subject, &read, &priority)
 		if err != nil {
-			w.Write([]byte(`{"status": 501, "error": "db error"}`))
+			w.Write([]byte(err.Error()))
 			return
 		}
-
-		items = append(items, it)
+		fmt.Printf("id=%d, subject=%s, read=%d, prio=%d, priomap=%s\n", id, subject, read, priority, PriorityMap[priority])
+		items = append(items, map[string]interface{}{"id": id, "subject": subject, "read": ReadMap[read], "priority": PriorityMap[priority]})
 	}
-
->>>>>>> b385bde38629b141a54824015f1fe72213a03965
-	result, _ := json.Marshal(items)
+	fmt.Println("message`s data in JSON")
+	result, _ := json.Marshal(map[string]interface{}{"messages": items})
 	w.Write(result)
 }
 
